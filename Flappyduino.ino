0d24bfc1efc3a941694f92c5fa40c5e12ff644a4
Angel Tello <Ing_mecatroni@hotmail.com>
2016-07-03 22:14:22 -0500

    Código Arduino

---
+++ Flappyduino.ino
@@ -0,0 +1,22 @@
+#include <Ultrasonic.h>
+ 
+Ultrasonic ultrasonic(12,11); // (Trig PIN,Echo PIN)
+int ultimaLectura=0, lecturaActual=0;
+ 
+void setup() 
+{
+  Serial.begin(9600); 
+}
+ 
+void loop()
+{ 
+  lecturaActual = ultrasonic.Ranging(CM);
+  if(lecturaActual > ultimaLectura + 20 || lecturaActual < ultimaLectura - 40)
+  {
+    Serial.print("{\"sensor\":\"");
+    Serial.print( lecturaActual );
+    Serial.print("\"}\n"); 
+    ultimaLectura = lecturaActual;
+  }
+  delay(10);
+}
